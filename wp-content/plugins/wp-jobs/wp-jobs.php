66b6ea1ad49923148a0e7269c83e9bdf61d92391
Oleg V. Gordeev <gordeevov@gmail.com>
2016-10-28 17:25:42 +0200

    28.10.2016 - 1

--- wp-content/plugins/wp-jobs/wp-jobs.php
+++ wp-content/plugins/wp-jobs/wp-jobs.php
@@ -29,4 +29,53 @@ function wp_jobs_uninstall(){
  
     //действие при удалении
 }
+
+//Удаляем лишнее из профиля
+
+function admin_del_options() {
+	global $_wp_admin_css_colors;
+	global $wp_rich_edit_exists;
+	 
+	$_wp_admin_css_colors = 0;
+	$wp_rich_edit_exists = 0;
+}
+ 
+add_action(‘admin_head’, ‘admin_del_options’);
+
+function true_remove_personal_options(){
+	echo "\n" . '<script type="text/javascript">
+	jQuery(document).ready(function($) {
+	$(\'form#your-profile > h2:first\').hide();
+	$(\'form#your-profile > table:first\').hide();
+	$(\'form#your-profile tr.user-url-wrap\').hide();
+	$(\'form#your-profile\').show(); });
+	</script>' . "\n";
+}
+ 
+add_action('admin_head', 'true_remove_personal_options');
+
+//Добавляем поля в профиль пользователя
+add_filter('user_contactmethods', 'my_user_contactmethods');
+
+function my_user_contactmethods($user_contactmethods)
+{
+    $user_contactmethods['tel'] = '<b>Телефон</b>'; 
+    $user_contactmethods['tel2'] = '<b>Телефон 2</b>';
+    $user_contactmethods['addr'] = '<b>Адрес</b>'; 
+    $user_contactmethods['birthdate'] = '<b>Дата рождения</b>';  
+	$user_contactmethods['education'] = '<b>Образование</b>';
+	$user_contactmethods['spec'] = '<b>Специальность</b>';
+	$user_contactmethods['experience'] = '<b>Опыт работы</b>';
+	$user_contactmethods['job_category'] = '<b>Виды деятельности</b>';
+	$user_contactmethods['facebook'] = '<b>Facebook</b>';
+	$user_contactmethods['vkontakte'] = '<b>ВКонтакте</b>';
+	$user_contactmethods['twitter'] = '<b>Twitter</b>';
+	$user_contactmethods['linkedin'] = '<b>LinkedIn</b>';
+	$user_contactmethods['instagram'] = '<b>Istagram</b>';
+	$user_contactmethods['skype'] = '<b>Skype</b>';
+	$user_contactmethods['viber'] = '<b>Viber</b>';
+	$user_contactmethods['whatsapp'] = '<b>WhatsApp</b>';
+
+    return $user_contactmethods;
+}
 ?>
